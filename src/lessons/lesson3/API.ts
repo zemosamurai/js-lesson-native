e9e955e1a9e9f1ea1f8cdfc94bba4adc5a3beac5
zemosamurai <zemosamurai@gmail.com>
2023-01-02 12:15:06 +0300

    add get from server

--- src/lessons/lesson3/API.ts
+++ src/lessons/lesson3/API.ts
@@ -3,13 +3,15 @@ import axios from 'axios';
 const configOMB = {
     baseURL: 'http://www.omdbapi.com',
 };
-const key = '';
+const key = 'bc11ef1f';
 const axiosInstance = axios.create(configOMB);
 
 const API = {
     searchFilmsByTitle: (title: string) => {
+        return axiosInstance.get(`/?t=${title}&apikey=${key}`)
     },
     searchFilmsByType: (title: string, type: string) => {
+        return axiosInstance.get(`/?t=${title}&type=${type}&apikey=${key}`)
     }
 };
 
